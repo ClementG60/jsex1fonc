26a1818fd74fb9e54805649798015bcdfe368812
ClementG60 <cle_guillaume@hotmail.fr>
2021-11-03 12:14:59 +0100

    ajout code

---
+++ ex1.js
@@ -0,0 +1,6 @@
+function askNumber(){
+    n1 = document.getElementById("idNumber1").value;
+    n2 = document.getElementById("idNumber2").value;
+    let result = n1 * n2
+    alert("Le resultat est : " + result)
+}
